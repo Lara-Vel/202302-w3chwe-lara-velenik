2f615c7ad02b828a6829d7a06f2d8f9bfd2856c8
Lara-Vel <lara.vel@outlook.com>
2023-02-04 18:53:16 +0100

    add index.js and add link js to html

---
+++ public/js/components/Component/Header/Header.js
@@ -0,0 +1,13 @@
+import Component from "../Component.js";
+export default class Header extends Component {
+    #logo;
+    constructor(text, parentElement, classCss = "") {
+        super(parentElement, "header", classCss);
+        this.#logo = text;
+    }
+    render() {
+        super.render();
+        this.element.innerHTML = `
+  <h1><img src="${this.#logo}" alt="pokemon logo"</h1>`;
+    }
+}
---
+++ public/js/index.js
@@ -0,0 +1,2 @@
+import App from "./components/Component/App/App.js";
+const app = new App(document.querySelector(".root")).render();
